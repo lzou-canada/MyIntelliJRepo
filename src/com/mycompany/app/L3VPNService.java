2ba1d69494710e2c73dbf2106e3dd94281c72dc1
lzou-canada <lizou.canada@gmail.com>
2017-02-25 16:11:30 -0500

    Added in the IP VPN service

---
+++ src/com/mycompany/app/L3VPNService.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+/**
+ * Created by Fei Chen on 2/25/2017.
+ */
+public class L3VPNService extends Service
+{
+    public L3VPNService(String name)
+    {
+        super(name, ServiceType.SERVICE_TYPE_L3VPN);
+    }
+}
