98f0924acaff102e602bf06e46fb7d60c020a3ff
lzou-canada <lizou.canada@gmail.com>
2017-02-25 15:15:26 -0500

    Added one line comment to push the Dev branch ahead

--- src/com/mycompany/app/CLineService.java
+++ src/com/mycompany/app/CLineService.java
@@ -5,6 +5,7 @@ package com.mycompany.app;
  */
 public class CLineService extends Service
 {
+    // add one line to push the Dev branch ahead
     // add one more line of comment to move the Dev branch another step forward
     // add one line of comment to move the Dev branch a step forward
     // add one line of comment to C-Line service
